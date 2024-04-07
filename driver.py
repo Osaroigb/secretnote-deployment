9b043942839d0b6d26334e375d8cb1192d437238
Osaro Igbinovia <emmyigbinovia@gmail.com>
2024-04-07 16:05:22 +0100

    fix: solve invalid argument bug

--- driver.py
+++ driver.py
@@ -1,8 +1,6 @@
 import os
 from selenium import webdriver
 from dotenv import load_dotenv
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 load_dotenv()
 
@@ -10,13 +8,10 @@ def setup_chrome_driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
-
-    chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--window-size=1250x600")
+
     chrome_options.add_argument("--disable-dev-shm-usage")
-    
     chrome_options.add_argument(f'user-agent={os.getenv("USER_AGENT")}')
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    return webdriver.Chrome(options=chrome_options)
